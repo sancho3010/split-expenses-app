fee006ec678f4d78bd60ce3a738d91a2a5ba549e
Santiago Higuita <santiago.higuitau@pragma.com.co>
2026-04-23 11:15:26 -0500

    fix: compact ci and add permissions to the frontend

--- src/backend/alembic/env.py
+++ src/backend/alembic/env.py
@@ -20,7 +20,7 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
+    """Run migrations in -offline- mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -40,9 +40,7 @@ def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
 
